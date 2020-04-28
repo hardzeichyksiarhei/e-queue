7aa6a242853c961a61e1105f21188351485fd92a
hardz <hardzeichyksiarhei@gmail.com>
2020-04-28 18:33:12 +0300

    ref: replace header text

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" className={classes.title} align="center">
-              Онлайн  очередь на ЦТ*
+              Регистрация на ЦТ* — онлайн очередь
             </Typography>
           </Toolbar>
         </AppBar>
