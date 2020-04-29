bd569c912ebfd9d3db43a87e47226f62582702d3
hardz <hardzeichyksiarhei@gmail.com>
2020-04-29 17:01:35 +0300

    fix: remove faceAuth

--- src/App.jsx
+++ src/App.jsx
@@ -22,18 +22,6 @@ const theme = createMuiTheme({
   },
 }, ruRU);
 
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    this.isAuthenticated = true
-    setTimeout(cb, 100) // fake async
-  },
-  signout(cb) {
-    this.isAuthenticated = false
-    setTimeout(cb, 100) // fake async
-  }
-}
-
 function RouteWrapper({
   component: Component,
   layout: Layout,
