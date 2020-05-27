ab20dd5ce81ca3173fd88505aa45522b79bc9884
hardz <hardzeichyksiarhei@gmail.com>
2020-05-27 22:54:13 +0300

    feat: add all hours bar statistics

--- src/components/admin/QueueDashboard/QueueAllTimeStats/QueueAllTimeStatsChart.jsx
+++ src/components/admin/QueueDashboard/QueueAllTimeStats/QueueAllTimeStatsChart.jsx
@@ -12,28 +12,55 @@ class QueueAllTimeStatsChart extends Component {
         this.state = {
             loading: true,
 
-            options: {
-                chart: { id: 'Распределение количества абитуриентов по датам' },
-                xaxis: { categories: [] },
-                legend: { show: false },
-                plotOptions: { bar: { distributed: true }, },
-                title: {
-                    text: 'Распределение количества абитуриентов по датам',
-                    align: 'center',
-                    margin: 10,
-                    offsetX: 0,
-                    offsetY: 0,
-                    floating: false,
-                    style: {
-                        fontSize: '24px',
-                        fontWeight: 'bold',
-                        fontFamily: undefined,
-                        color: '#263238'
+            allDays: {
+                options: {
+                    chart: { id: 'Распределение абитуриентов по датам' },
+                    xaxis: { categories: [] },
+                    legend: { show: false },
+                    plotOptions: { bar: { distributed: true }, },
+                    title: {
+                        text: 'Распределение абитуриентов по датам',
+                        align: 'center',
+                        margin: 10,
+                        offsetX: 0,
+                        offsetY: 0,
+                        floating: false,
+                        style: {
+                            fontSize: '24px',
+                            fontWeight: 'bold',
+                            fontFamily: undefined,
+                            color: '#263238'
+                        },
                     },
+                    theme: { mode: 'light', palette: 'palette2' }
                 },
-                theme: { mode: 'light', palette: 'palette2' }
+                series: [{ name: undefined, data: [] }]
             },
-            series: [{ name: undefined, data: [] }]
+
+            allHours: {
+                options: {
+                    chart: { id: 'Распределение абитуриентов по времени' },
+                    xaxis: { categories: [] },
+                    legend: { show: false },
+                    plotOptions: { bar: { distributed: true }, },
+                    title: {
+                        text: 'Распределение абитуриентов по времени',
+                        align: 'center',
+                        margin: 10,
+                        offsetX: 0,
+                        offsetY: 0,
+                        floating: false,
+                        style: {
+                            fontSize: '24px',
+                            fontWeight: 'bold',
+                            fontFamily: undefined,
+                            color: '#263238'
+                        },
+                    },
+                    theme: { mode: 'light', palette: 'palette2' }
+                },
+                series: [{ name: undefined, data: [] }]
+            }
         }
     }
 
@@ -43,19 +70,32 @@ class QueueAllTimeStatsChart extends Component {
 
     async _fetchNunmberOfUsers() {
         try {
-            const { labels, values } = await userServices.fetchNunmberOfUsers();
+            const { labels, values, hoursLabels, hoursValues } = await userServices.fetchNunmberOfUsers();
             
             this.setState({
                 ...this.state,
-                options: {
-                    ...this.state.options,
-                    xaxis: { categories: labels },
-                    title: {
-                        ...this.state.options.title,
-                        text: `Распределение количества абитуриентов по датам (${values.reduce((a, b) => a + b)})`
-                    }
+                allDays: {
+                    options: {
+                        ...this.state.allDays.options,
+                        xaxis: { categories: labels },
+                        title: {
+                            ...this.state.allDays.options.title,
+                            text: `Распределение абитуриентов по датам (${values.reduce((a, b) => a + b)})`
+                        }
+                    },
+                    series: [{ name: 'Количество абитуриентов', data: values }],
                 },
-                series: [{ name: 'Количество абитуриентов', data: values }],
+                allHours: {
+                    options: {
+                        ...this.state.allHours.options,
+                        xaxis: { categories: hoursLabels },
+                        title: {
+                            ...this.state.allHours.options.title,
+                            text: `Распределение абитуриентов по времени (${hoursValues.reduce((a, b) => a + b)})`
+                        }
+                    },
+                    series: [{ name: 'Количество абитуриентов', data: hoursValues }],
+                },
                 loading: false
             });
         } catch (e) { console.error(e); }
@@ -65,11 +105,19 @@ class QueueAllTimeStatsChart extends Component {
         if (this.state.loading) return <div className="dashboard-loading"><CircularProgress /></div>
 
         return (
-            <Grid container spacing={3} justify="center" alignItems="flex-start">
-                <Grid item xs={12} md={8}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} md={6}>
                     <Chart
-                        options={this.state.options}
-                        series={this.state.series}
+                        options={this.state.allDays.options}
+                        series={this.state.allDays.series}
+                        type="bar"
+                        width="100%"
+                    />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Chart
+                        options={this.state.allHours.options}
+                        series={this.state.allHours.series}
                         type="bar"
                         width="100%"
                     />
